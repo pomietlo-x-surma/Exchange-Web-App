5b794438a47e0f02ffeb7ebed04dbe3506219bf9
Surma <kinga.surma04@gmail.com>
2024-07-25 19:28:23 +0200

    Update handling_CSV_file.h

--- Pass-Checker/handling_CSV_file.h
+++ Pass-Checker/handling_CSV_file.h
@@ -1,3 +1,4 @@
 ﻿#include <iostream>
 
-; bool WriteLogsToFile(const std::string& file_path, const std::string& email, const std::string& login, const std::string& password);
+bool WriteLogsToFile(const std::string& file_path, const std::string& email, const std::string& login, const std::string& password);
+bool correct_password_check(const std::string& file_path, const std::string& email, const std::string& input_login, const std::string& input_pass);
