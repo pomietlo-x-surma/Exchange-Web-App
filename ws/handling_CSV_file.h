6335451bd7df5de30f14c3be9b515c7292f3fd32
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-07-31 22:06:33 +0200

    Update handling_CSV_file.h

--- ws/handling_CSV_file.h
+++ ws/handling_CSV_file.h
@@ -4,5 +4,6 @@
 #include <vector>
 
 bool WriteLogsToFile(const std::string& file_path, const std::string& email, const std::string& login, const std::string& password);
-bool correct_password_check(const std::string& input_login, const std::string& input_pass);
+bool correct_password_check(const std::string& input_email, const std::string& input_pass);
 bool EnsureFileExists(const std::string& file_path);
+bool check_login_email_existence(const std::string email, const std::string& login);
