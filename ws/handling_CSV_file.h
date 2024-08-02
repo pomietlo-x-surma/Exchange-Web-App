af7fce51308c323c7c6b432c7f8410b6817d98c3
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-08-02 18:19:24 +0200

    Update handling_CSV_file.h

--- ws/handling_CSV_file.h
+++ ws/handling_CSV_file.h
@@ -1,9 +1,8 @@
 #pragma once
 
 #include <iostream>
-#include <vector>
 
-bool WriteLogsToFile(const std::string& email, const std::string& login, const std::string& password);
+bool WriteLogsToFile(const std::string& email, const std::string& login, const std::string& password, const std::string& file_path = "Dane.csv");
 bool correct_password_check(const std::string& input_email, const std::string& input_pass);
-bool EnsureFileExists(const std::string& file_path);
+//bool EnsureFileExists(const std::string& file_path);
 bool check_login_email_existence(const std::string email, const std::string& login);
