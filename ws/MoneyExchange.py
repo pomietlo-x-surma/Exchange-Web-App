0dcfeb90bd9107fc985ca911621112d82f0230b4
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-08-07 13:59:23 +0200

    Create MoneyExchange.py

---
+++ ws/MoneyExchange.py
@@ -0,0 +1,41 @@
+import requests as r
+from bs4 import BeautifulSoup
+import sys
+
+curr = {
+    'zloty': ('PLN', 'zł'),
+    'dollar': ('USD', '$'),
+    'euro': ('EUR', '€'),
+    'pound': ('GBP', '£'),
+    'hryvna': ('UAH', '₴'),
+    'dinar': ('TND', 'DT')
+}
+
+
+def get_currency(currency1, currency2):
+    try:
+        if currency1 == currency2:
+            return curr[currency2][1]
+        if type(currency1) is not str or type(currency2) is not str:
+            raise TypeError("Zły typ danych!")
+        else:
+            url = f'https://www.google.com/finance/quote/{curr[currency1][0]}-{curr[currency2][0]}'
+            page = r.get(url)
+            soup = BeautifulSoup(page.text, 'html.parser')
+            res = soup.find_all('div', attrs={'class': 'YMlKec fxKbKc'})
+            res = float([x.text for x in res][0].replace(',', ''))
+            return res, currency2
+    except ValueError:
+        raise ValueError("Wystąpił błąd!")
+    except Exception as e:
+        print(f"Błąd: {e}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Użycie: MoneyExchange.exe [currency1] [currency2]")
+    else:
+        currency1 = sys.argv[1].lower()
+        currency2 = sys.argv[2].lower()
+        result = get_currency(currency1, currency2)
+        print(f"{result[0]}")
