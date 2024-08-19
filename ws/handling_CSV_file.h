4328123b778a757267e5d6a7dbbd1aa37e2487bf
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-08-19 18:55:35 +0200

    Update handling_CSV_file.h

--- ws/handling_CSV_file.h
+++ ws/handling_CSV_file.h
@@ -2,12 +2,8 @@
 
 #include <iostream>
 
-//login, email, haslo -> Dane.csv
 void WriteLogsToFile_Passes(const std::string& email, const std::string& login, const std::string& password, const std::string& file_path = "Dane.csv");
-
-//info o walutach -> Users.csv
-void WriteLogsToFile_Currencies(const std::string& login, const std::string& euro, const std::string& dolar, const std::string& zloty, const std::string& file_path = "Users.csv", bool reg = false);
-
+void WriteLogsToFile_Currencies(const std::string& login, const std::string& dolar, const std::string& euro, const std::string& zloty, const std::string& file_path = "Users.csv", bool reg = false);
 std::string ReadLogs(const std::string& login, const std::string& file_path = "Users.csv");
-bool correct_password_check(const std::string& input_email, const std::string& input_pass);
-bool check_login_email_existence(const std::string& email, const std::string& login);
+std::string correct_password_check(const std::string& input_email, const std::string& input_pass, const std::string& file_path = "Dane.csv");
+bool check_login_email_existence(const std::string& email, const std::string& login, const std::string& file_path = "Dane.csv");
