e5ba764a427b098e0eb46f68540cd26a053e735b
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-08-24 16:28:53 +0200

    Update handling_CSV_file.cpp

--- handling_CSV_file.cpp
+++ handling_CSV_file.cpp
@@ -2,77 +2,81 @@
 #include <fstream>
 #include <string>
 #include <iostream>
-#include "handling_CSV_file.h"
+#include "handling_CSV_file.hpp"
+#include "money_converter.hpp"
 #include <vector>
 #include <array>
-#include "money_converter.h"
 #include <unordered_map>
+#include <thread>
+
 
 void Currency_gen() {
-	std::cout << "szim\n";
-	std::ofstream outfile("currencies.csv", std::ios_base::app);
-	std::array<std::string, 3> currencies = { "dollar", "euro", "zloty" };
-	std::unordered_map<std::string, std::string> currencies2 = { {"dollar", "USD"}, {"euro","EUR"}, {"zloty", "PLN"} };
-	for (const auto& first : currencies) {
-		for (const auto& second : currencies) {
-			if (first != second) {
-				std::string c1 = currency_comparison(first, second);
-				std::string c2 = currency_comparison(first, second, true);
-				outfile << currencies2[first] << " " << currencies2[second] << ',' << c1 + " " + c2 << '\n';
+		std::cout << "szim\n";
+		std::ofstream outfile("currencies.csv", std::ios_base::app);
+		std::array<std::string, 3> currencies = { "dollar", "euro", "zloty" };
+		std::unordered_map<std::string, std::string> currencies2 = { {"dollar", "USD"}, {"euro","EUR"}, {"zloty", "PLN"} };
+		for (const auto& first : currencies) {
+			for (const auto& second : currencies) {
+				if (first != second) {
+					std::string c1 = currency_comparison(first, second);
+					std::string c2 = currency_comparison(first, second, true);
+					outfile << currencies2[first] << " " << currencies2[second] << ',' << c1 + " " + c2 << '\n';
+				}
 			}
 		}
-	}
-	outfile.close();
+		outfile.close();
 }
 
 
 
 
 void Currency_update() {
-	bool found = false;
-	std::ifstream infile("currencies.csv");
-	std::vector<std::string> lines;
-	std::string line;
-	std::array<std::string, 3> currencies = { "dollar", "euro", "zloty" };
-	std::unordered_map<std::string, std::string> currencies2 = { {"dollar", "USD"}, {"euro","EUR"}, {"zloty", "PLN"} };
-	for (const auto& first : currencies) {
-		for (const auto& second : currencies) {
-			if (first != second) {
-				while (std::getline(infile, line)) {
-					std::stringstream ss(line);
-					std::string line_first, line_second;
-					ss >> line_first >> line_second;
+	while (true) {
+		bool found = false;
+		std::ifstream infile("currencies.csv");
+		std::vector<std::string> lines;
+		std::string line;
+		std::array<std::string, 3> currencies = { "dollar", "euro", "zloty" };
+		std::unordered_map<std::string, std::string> currencies2 = { {"dollar", "USD"}, {"euro","EUR"}, {"zloty", "PLN"} };
+		for (const auto& first : currencies) {
+			for (const auto& second : currencies) {
+				std::cout << "currency updated!\n";
+				std::this_thread::sleep_for(std::chrono::seconds(10));
+				if (first != second) {
+					while (std::getline(infile, line)) {
+						std::stringstream ss(line);
+						std::string line_first, line_second;
+						ss >> line_first >> line_second;
 
-					if (line_first == first && line_second == second) {
-						std::string c1 = currency_comparison(first, second);
-						std::string c2 = currency_comparison(first, second, true);
-						std::string c1_new = currencies2[c1];
-						std::string c2_new = currencies2[c2];
-						std::string result = first + " " + second + c1_new + " " + c2_new + '\n';
-						lines.push_back(result);
-						found = true;
-					}
-					else {
-						lines.push_back(line);
+						if (line_first == first && line_second == second) {
+							std::string c1 = currency_comparison(first, second);
+							std::string c2 = currency_comparison(first, second, true);
+							std::string c1_new = currencies2[c1];
+							std::string c2_new = currencies2[c2];
+							std::string result = first + " " + second + c1_new + " " + c2_new + '\n';
+							lines.push_back(result);
+							found = true;
+						}
+						else {
+							lines.push_back(line);
+						}
 					}
 				}
 			}
 		}
+		infile.close();
+		std::ofstream outfile("currencies.csv", std::ios_base::trunc);
+		for (const auto& updated_line : lines) {
+			outfile << updated_line << '\n';
+		}
+		outfile.close();
 	}
-	infile.close();
-
-	// Zapis zaktualizowanych danych do pliku
-	std::ofstream outfile("currencies.csv", std::ios_base::trunc);
-	for (const auto& updated_line : lines) {
-		outfile << updated_line << '\n'; // Zapisz linię
-	}
-	outfile.close();
 }
 
 
 void WriteLogsToFile_Passes(const std::string& email, const std::string& login, const std::string& password, const std::string& file_path) {
 	std::ofstream file(file_path, std::ios_base::app);
-	file << email << ' ' << login << ' ' << password << '\n';
+	file << email << ' ' << login  << ' ' << password << '\n';
 	file.close();
 }
 
@@ -82,7 +86,7 @@ void WriteLogsToFile_Currencies(const std::string& login, const std::string& dol
 	const std::string& euro, const std::string& zloty, const std::string& file_path, bool reg) {
 
 	std::string new_entry = login + ',' + dolar + ' ' + euro + ' ' + zloty + '\n';
-
+	std::cout << new_entry << '\n';
 	if (reg) {
 		std::ofstream file(file_path, std::ios_base::app);
 		file << new_entry;
@@ -97,10 +101,11 @@ void WriteLogsToFile_Currencies(const std::string& login, const std::string& dol
 		while (std::getline(infile, line)) {
 			std::stringstream ss(line);
 			std::string login_infile;
-			ss >> login_infile;
-
+			std::getline(ss, login_infile, ',');
+			std::cout << login_infile << "Z\n";
 			if (login_infile == login) {
-				lines.push_back(new_entry);
+				std::cout << line << "\n";
+				lines.push_back(new_entry + "X");
 				found = true;
 			}
 			else {
@@ -129,9 +134,7 @@ std::string ReadLogs(const std::string& login, const std::string& file_path) {
 		std::stringstream ss(line);
 		std::string login_infile;
 		std::getline(ss, login_infile, ',');
-		std::cout << "X" << login << "X" << " ///X" << login_infile << "X\n";
 		std::getline(ss, log);
-		std::cout << log << std::endl;
 		if (login_infile == login) {
 			std::getline(ss, log);
 			return log;
