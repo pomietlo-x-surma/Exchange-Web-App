cb24917d2feefdcb106ffe32903da6e21fdb8a3b
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-08-26 18:10:16 +0200

    Update money_converter.hpp

--- src/modules/money_converter.hpp
+++ src/modules/money_converter.hpp
@@ -1,4 +1,5 @@
+#pragma once
 #include <iostream>
 
 std::string exec(const char* cmd);
-std::string currency_comparison(const std::string& currency1,const std::string& currency2, bool chart=false);
+std::string currency_comparison(const std::string& currency1, const std::string& currency2, bool chart = false);
