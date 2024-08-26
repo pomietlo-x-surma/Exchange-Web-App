8871c2f9135f74f10780a7435f4aa2e87022ee66
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-08-26 14:29:40 +0200

    Update server_message_processing.hpp

--- src/modules/server_message_processing.hpp
+++ src/modules/server_message_processing.hpp
@@ -1,1 +1,13 @@
+#pragma once
+#include <iostream>
 
+std::string to_string_with_precision(const long double& value, const char& precision = 2);
+std::string process_message(const std::string& received_message);
+std::string check_register(const std::string& email, const std::string& login, const std::string& pass, const std::string& pass_rep);
+std::string check_logging(const std::string& email, const std::string& pass);
+std::string login_user(const std::string& message);
+std::string register_user(const std::string& message);
+std::string exchange_rate(const std::string& message);
+std::string account_balance(const std::string& message);
+std::string exchange(const std::string& message);
+std::string receive_text(const char& id, const std::string& message);
