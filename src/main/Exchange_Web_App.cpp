6136517f54415955afa198f2641fb7d8ff80c0bd
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-08-26 18:08:23 +0200

    Update Exchange_Web_App.cpp

--- src/main/Exchange_Web_App.cpp
+++ src/main/Exchange_Web_App.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <thread>
-
-#include "server_file.hpp"
-#include "handling_CSV_file.hpp"
-
+#include "../modules/server_file.hpp"
+//#include <thread>
+//#include "../modules/handling_CSV_file.hpp"
 
 
-int main() {
+int main()
+{
 	//Currency_gen(); //generacja nowych walut
 	//std::thread t1(Currency_update); //aktualizacja kursów 
 	serwer();
