f7cb84ef9e78e3c15a08a8f003afa7343690dd6a
Surma <kinga.surma04@gmail.com>
2024-08-31 15:43:55 +0200

    Update test_money_converter.cpp

--- tests/test_money_converter.cpp
+++ tests/test_money_converter.cpp
@@ -1,24 +1,21 @@
-#define BOOST_TEST_DYN_LINK
 #include <boost/test/included/unit_test.hpp>
-#include "../src/modules/money_converter.cpp"
+#include "../src/modules/money_converter.cpp"  
 
 std::string mock_exec(const char* cmd) {
-	return "4.56 USD";
+    return "4.56 chart";
 }
 
 BOOST_AUTO_TEST_CASE(TestCurrencyComparisonWithMockExec) {
     exec_func = mock_exec;
-    
+
     std::string currency1 = "PLN";
     std::string currency2 = "USD";
 
     std::string result = currency_comparison(currency1, currency2, false);
-	BOOST_CHECK_EQUAL(result, "4.56");
+    BOOST_CHECK_EQUAL(result, "4.56");
 
-    std::code_result(currency1, currency2, true);
-    BOOST_CHECK_EQUAL(code_result, "USD");
+    std::string code_result = currency_comparison(currency1, currency2, true);
+    BOOST_CHECK_EQUAL(code_result, "chart");
 
     exec_func = exec;
-
 }
-
