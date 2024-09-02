f46ef1f450c0ff5591b6647b00bce13ad16d4c05
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-09-02 21:49:24 +0200

    Update test_server_message_processing.cpp

--- tests/test_server_message_processing.cpp
+++ tests/test_server_message_processing.cpp
@@ -1,6 +1,8 @@
+#pragma once
+
 #define BOOST_TEST_MODULE AppTest
 #include <boost/test/included/unit_test.hpp>
-#include "../src/modules/server_message_processing.cpp"
+#include "../src/modules/server_message_processing.hpp"
 
 BOOST_AUTO_TEST_CASE(TestPositiveNumber) {
     BOOST_CHECK_EQUAL(to_string_with_precision(2.312, 1), "2.3");
