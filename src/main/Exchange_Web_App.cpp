a45bb466c2f14adb0d5270d36c8793b704eb6678
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-09-02 23:11:07 +0200

    Update Exchange_Web_App.cpp

--- src/main/Exchange_Web_App.cpp
+++ src/main/Exchange_Web_App.cpp
@@ -4,7 +4,6 @@
 #include "../modules/handling_CSV_file.hpp"
 
 int main(){
-	//std::thread t1(Currency_gen); //generating new currencies to currencies.csv
 	//std::thread t1(Currency_update); //updating currencies in currencies.csv
 	server(); //turning on server
 }
