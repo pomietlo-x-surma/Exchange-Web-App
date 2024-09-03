14753b09224f9034aa642bf531304ab87d9b5b3b
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-09-03 20:29:39 +0200

    Update Exchange_Web_App.cpp

--- src/main/Exchange_Web_App.cpp
+++ src/main/Exchange_Web_App.cpp
@@ -2,8 +2,25 @@
 #include "../modules/server_file.hpp"
 #include <thread>
 #include "../modules/handling_CSV_file.hpp"
+#include <sqlite3.h>
 
-int main(){
-	//std::thread t1(Currency_update); //updating currencies in currencies.csv
-	server(); //turning on server
+//int main(){
+//	//std::thread t1(Currency_update); //updating currencies in currencies.csv
+//	server(); //turning on server
+//}
+
+int main() {
+    sqlite3* db;
+    int rc = sqlite3_open("example.db", &db);
+
+    if (rc) {
+        std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
+        return rc;
+    }
+    else {
+        std::cout << "Opened database successfully." << std::endl;
+    }
+
+    sqlite3_close(db);
+    return 0;
 }
