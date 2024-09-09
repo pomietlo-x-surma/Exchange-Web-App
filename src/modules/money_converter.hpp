86ea7f516bf9b83e5a34d4757b69eb2d2c717acc
Surma <kinga.surma04@gmail.com>
2024-09-09 22:05:07 +0200

    Update money_converter.hpp

--- src/modules/money_converter.hpp
+++ src/modules/money_converter.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
 
+extern std::function<std::string(const char* cmd)> exec_func;
 std::string exec(const char* cmd);
 std::string currency_comparison(const std::string& currency1, const std::string& currency2, bool chart = false);
