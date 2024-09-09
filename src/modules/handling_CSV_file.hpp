bc5d4db893ec19663c11089e81ae4975bb0a71b5
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-09-09 16:04:25 +0200

    Update handling_CSV_file.hpp

--- src/modules/handling_CSV_file.hpp
+++ src/modules/handling_CSV_file.hpp
@@ -5,9 +5,10 @@ constexpr auto path_to_user_balance_csv = R"(../database/user_balance.csv)";
 constexpr auto path_to_currencies_csv = R"(../database/currencies.csv)";
 constexpr auto path_to_database_db = R"(../database/database.db)";
 
+
 void write_logs_to_file_user_auth(const std::string& email, const std::string& login, const std::string& password, const std::string& file_path = path_to_user_auth_csv);
-void write_logs_to_file_user_balance(const std::string& login, const std::string& dollar, const std::string& euro, const std::string& zloty, bool reg = false);
-std::string read_logs_user_auth(const std::string& logs);
+void updating_user_balance(const std::string& login, const std::string& dollar, const std::string& euro, const std::string& zloty);
+void appending_user_balance(const std::string& login, const std::string& dollar, const std::string& euro, const std::string& zloty);
 std::string read_logs_currencies(const std::string& logs);
 std::string read_logs_user_balance(const std::string& logs);
 std::string correct_password_check(const std::string& input_email, const std::string& input_pass, const std::string& file_path = path_to_user_auth_csv);
