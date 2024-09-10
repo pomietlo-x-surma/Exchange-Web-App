3370a5aaf47e0f431962f8be201ee9944fe62797
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-09-10 14:30:02 +0200

    Delete out/build/x64-Debug directory

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/source/repos/Exchange-Web-App
-# Build directory: C:/Users/User/source/repos/Exchange-Web-App/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/boost-build")
-subdirs("tests")
