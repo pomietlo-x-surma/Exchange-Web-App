3e03b4f98e81b0a8d26bc72d178cef73b2d70ebd
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-09-15 21:54:04 +0100

    Update MainWrapper.tsx

--- React/src/components/MainWrapper.tsx
+++ React/src/components/MainWrapper.tsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 import Main from "./Main";
 
+const client = new W3CWebSocket("ws://localhost:8080");
+
 const MainWrapper: React.FC = () => {
-  const username = localStorage.getItem("username") || ""; // Pobieramy username z localStorage
+  const username = localStorage.getItem("username") || "";
+
+  useEffect(() => {
+    // Sprawdź, czy połączenie WebSocket jest otwarte
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(`User ${username} has switched to Main page.`);
+    } else {
+      // Jeśli połączenie nie jest otwarte, ustaw nasłuch na `onopen`
+      client.onopen = () => {
+        client.send(`User ${username} has switched to Main page.`);
+      };
+    }
+
+    // Opcjonalne odświeżenie strony po 0.5s
+    setTimeout(() => {
+      window.location.reload();
+    }, 500);
+
+    // Funkcja czyszcząca po odmontowaniu komponentu
+    return () => {
+      client.onopen = null;
+    };
+  }, [username]);
 
   return <Main username={username} />;
 };
