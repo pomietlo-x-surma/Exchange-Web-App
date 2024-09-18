82e5d136036dd20a08281723a8b235c6124e742b
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-09-18 14:51:15 +0200

    Update Exchange_Web_App.cpp

--- src/main/Exchange_Web_App.cpp
+++ src/main/Exchange_Web_App.cpp
@@ -1,13 +1,7 @@
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include "../modules/server_file.hpp"
-#include <thread>
 #include "../modules/handling_CSV_file.hpp"
-#include <string>
-#include "sqlite3.h"
+
 
 int main() {
-    currency_generation(); //updating currencies in currencies.csv
-    server(); //turning on server
+    //currency_generation(); //updating currencies in currencies.csv
+    //server(); //turning on server
 }
