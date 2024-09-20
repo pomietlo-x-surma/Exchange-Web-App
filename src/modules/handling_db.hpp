ac35f97551e316ef7a141be5d9dd006269436417
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-09-20 15:50:05 +0200

    Update handling_db.hpp

--- src/modules/handling_db.hpp
+++ src/modules/handling_db.hpp
@@ -1,10 +1,6 @@
 #pragma once
 #include <iostream>
-constexpr auto path_to_user_auth_csv = R"(../database/user_auth.csv)";
-constexpr auto path_to_user_balance_csv = R"(../database/user_balance.csv)";
-constexpr auto path_to_currencies_csv = R"(../database/currencies.csv)";
 constexpr auto path_to_database_db = R"(../database/database.db)";
-
 
 void write_logs_to_file_user_auth(const std::string& email, const std::string& login, const std::string& password, const std::string& file_path = path_to_user_auth_csv);
 void updating_user_balance(const std::string& login, const std::string& dollar, const std::string& euro, const std::string& zloty);
