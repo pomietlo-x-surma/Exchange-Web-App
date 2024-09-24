056fdb1a313f4ada728442aacc89afda613a2efb
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-09-24 20:41:00 +0200

    Update test_server_message_processing.cpp

--- tests/test_server_message_processing.cpp
+++ tests/test_server_message_processing.cpp
@@ -2,5 +2,8 @@
 #include "../src/modules/server_message_processing.hpp"
 
 BOOST_AUTO_TEST_CASE(TestPositiveNumber) {
-    BOOST_CHECK_EQUAL(to_string_with_precision(2.312, 1), "2.3");
+    BOOST_REQUIRE_EQUAL(to_string_with_precision(2.312, 1), "2.3");
+}
+BOOST_AUTO_TEST_CASE(TestPasswordEquality){
+	BOOST_REQUIRE_EQUAL(check_register("adam123@o2.pl", "adam", "Admin12345!", "Admin12345!"), "0adam");
 }
