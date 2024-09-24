043d191691b140efa8ff253fdce87c58bd436d60
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-09-24 21:13:20 +0200

    Update test_server_message_processing.cpp

--- tests/test_server_message_processing.cpp
+++ tests/test_server_message_processing.cpp
@@ -1,9 +1,23 @@
 #include <boost/test/unit_test.hpp>
 #include "../src/modules/server_message_processing.hpp"
+#include "../src/modules/password_checker.hpp"
+
+struct TestUser {
+	std::string email = "bart11@o2.pl";
+	std::string login = "bart";
+	std::string password = "qWERTY1!!!";
+}user;
+
+
 
 BOOST_AUTO_TEST_CASE(TestPositiveNumber) {
     BOOST_REQUIRE_EQUAL(to_string_with_precision(2.312, 1), "2.3");
 }
+
+
+
+
 BOOST_AUTO_TEST_CASE(TestPasswordEquality){
-	BOOST_REQUIRE_EQUAL(check_register("adam123@o2.pl", "adam", "Admin12345!", "Admin12345!"), "0adam");
+	BOOST_REQUIRE_EQUAL(check_register("1"+user.email, user.login+"1", user.password, user.password), "The specified email or login already exists!");
+	BOOST_REQUIRE_EQUAL(check_register("1aa"+user.email, user.login+"111", user.password, user.password+'1'), "Passwords don't match!");
 }
